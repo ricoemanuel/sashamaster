2893cc9d56d661960e31b6f441faf80223f56c34
Santiago <santiosorio1357@gmail.com>
2023-04-07 20:39:51 -0500

    Create home page and registration and login buttons

---
+++ lib/views/homePage.view.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class InicioPage extends StatelessWidget {
+  const InicioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            height: 150,
+            width: double.infinity,
+            margin: const EdgeInsets.all(16),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/images/logo_universidad.png'),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Centro educativo donde la libertad y la ciencia fueran los pilares que soportaran la vida académica. ',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 200,
+                  width: 350,
+                  margin: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                      image: AssetImage('lib/images/campus.png'),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Navegar a la pantalla de registro
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.red),
+                        ),
+                        child: const Text('Registro',
+                            style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 150,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Navegar a la pantalla de registro
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.red),
+                        ),
+                        child:
+                            const Text('Login', style: TextStyle(fontSize: 20)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
