c213baaae5e8b71ea4a61c026e1b5b43f753a2ce
Santiago <santiosorio1357@gmail.com>
2023-05-28 11:13:43 -0500

    Design semesters students

--- lib/views/terms.view.dart
+++ lib/views/terms.view.dart
@@ -24,49 +24,66 @@ class _termState extends State<term> {
         if (snapshot.hasData) {
           final data = snapshot.data!;
           final terms = data["term"];
-          final List<Widget> items = List.generate(
-            terms,
-            (index) => GestureDetector(
-              onTap: () {
-                setState(() {
-                  selectedSemesterIndex = index;
-                });
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => termDetail(selectedSemesterIndex,data),
-                  ),
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.0),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                ),
-                margin: const EdgeInsets.only(top: 16.0),
-                child: SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: ListTile(
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 16),
-                    title: Text(
-                      'Semestre ${index + 1}',
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 24,
+          final List<Widget> rows = List.generate(
+            (terms / 2).ceil(),
+            (rowIndex) {
+              final int startIndex = rowIndex * 2;
+              final int endIndex = startIndex + 1;
+              final List<Widget> rowItems = List.generate(
+                2,
+                (columnIndex) {
+                  final index = startIndex + columnIndex;
+                  if (index < terms) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedSemesterIndex = index;
+                        });
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                termDetail(selectedSemesterIndex, data),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16.0),
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 1.0,
+                          ),
+                        ),
+                        margin: const EdgeInsets.only(top: 16.0, right: 16.0),
+                        width: 150,
+                        height: 100,
+                        child: ListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 16),
+                          title: Text(
+                            'Semestre ${index + 1}',
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
+                },
+              );
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: rowItems,
+              );
+            },
           );
           return ListView(
-            children: items,
+            children: rows,
           );
         } else if (snapshot.hasError) {
           return const Text('Error al obtener los datos');
